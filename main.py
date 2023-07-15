ac8cf7519dd4cae58227dadd24ec01de9829ae70
Squuidd <reesecooper1010@gmail.com>
2023-07-15 00:18:16 -0700

    UI for adding template

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
 import PySimpleGUI as sg
-
 import json
+import window as wd
+
+class Template:
+    def __init__(self, name):
+        self.name = name
+    
+
+    def create_txt(self):
+        pass
+        # create path and variable for path
+
+    pass
+
+
+
+window = wd.main()
+
+# scan for templates
+
+
+
+while True:
+    event, values = window.read()
+
+    if event == sg.WIN_CLOSED:
+        break
+
+    elif event == "add_template":
+        template_name = wd.name_input()
+        print(window)
+        #window.extend_layout(window['new_column'], wd.template_button(template_name))
+
+
+
+
+window.close()
+
--- window.py
+++ window.py
@@ -1,1 +1,50 @@
 import PySimpleGUI as sg
+
+WINDOW_NAME = "Email Template Tool"
+font = ("Arial", 14)
+
+def main():
+    column_layout = []
+
+    layout = [
+        [
+            sg.Button("Add Template", key="add_template", font=font)
+        ],
+        [
+            sg.Column(column_layout, key="new_column") 
+        ]
+    ]
+    
+    return sg.Window(WINDOW_NAME, layout)
+
+def name_input():
+    name_field = [
+        [
+            sg.InputText(key="name", enable_events=True, size=(400, 90), font=font)
+        ]
+    ]
+
+    layout = [
+        [
+            sg.Frame("Enter Template Name: ", layout=name_field, size=(500, 52), font=font),
+            sg.Button("Submit", key="template_name", font=("Arial", 16))
+        ]
+    ]
+
+    window = sg.Window(WINDOW_NAME, layout)
+
+    while True:
+        event, values = window.read()
+
+        if event == "template_name":
+            window.close()
+            return name_field[0][0].get()
+
+        if event == sg.WIN_CLOSED:
+            break
+
+    window.close()
+
+def template_button(name):
+    return sg.Button(name, key=f"{name}_select", font=font)
+    
